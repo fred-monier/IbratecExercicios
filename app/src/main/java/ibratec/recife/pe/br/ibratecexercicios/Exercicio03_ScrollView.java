ed2918f9fc1248d99ba3c21e296fdba45bc444fa
fred-monier <frederico.monier@gmail.com>
2017-09-19 17:31:52 -0300

    Exercícios. Em 19/09/2017

---
+++ app/src/main/java/ibratec/recife/pe/br/ibratecexercicios/Exercicio03_ScrollView.java
@@ -0,0 +1,13 @@
+package ibratec.recife.pe.br.ibratecexercicios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercicio03_ScrollView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicio03__scroll_view);
+    }
+}
